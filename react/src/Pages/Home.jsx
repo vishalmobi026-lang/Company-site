f9c8db0e976a7500452274eb1f454430047b50f4
Developer <developer@example.com>
2026-04-27 05:24:04 -0700

    Add project files and dependencies, ignore node_modules

--- react/src/Pages/Home.jsx
+++ react/src/Pages/Home.jsx
@@ -1,31 +1,49 @@
 import Navbar from "../components/Navbar";
+import knowl from "../Assets/knowl.jpg";
 function Home(){
     return(
         <>
 <div style={{
       height: "100%",
-        backgroundColor: "lightgray",
+        backgroundColor: "#F0FFF0",
 }}>
     <div>
-<h1 style={{textAlign:"center",paddingTop:"20px",fontFamily:"Ariel",fontSize:"50px"}}>
-    welcome to this site 
-</h1>
-<p style={{padding:"200px",fontFamily:"Ariel",fontSize:"50px"}} >this is the site for learning new things and train you brin with knowledge <br />lerom20
-this is the site for learning new things and train you brin with knowledge <br />
-this is the site for learning new things and train you brin with knowledge <br /> this is the site for learning new things and train you brin with knowledge</p>
-</div>
-<div style={{padding:"100px",display:"flex",width:"200px",height:"200px",backgroundColor:"orange"}}>
+<div
+  className="container"
+  style={{
+    backgroundImage: `url(${knowl})`,
+    backgroundSize: "cover",
+    backgroundPosition: "center",
+    backgroundRepeat: "no-repeat",
+    padding: "60px",
+    paddingBottom: "200px",
+    minHeight: "100vh",
+    color: "white",
+    backgroundColor: "rgba(0,0,0,0.4)",
+    backgroundBlendMode: "darken"
+  }}
+>
+  <h1
+    style={{
+      textAlign: "center",
+      paddingTop: "20px",
+      fontFamily: "Arial",
+      fontSize: "50px",
+      color: "white",
+backgroundColor: "rgba(0,0,0,0.4)",
+backgroundBlendMode: "darken",
+    }}
+  >
+    Welcome to this site
+  </h1>
 
+  <p style={{ fontSize: "24px", lineHeight: "1.6" }}>
+    This platform is designed to help individuals learn new skills and expand
+    their knowledge in a practical and engaging way. Our mission is to provide
+    high-quality educational resources that support continuous learning and
+    professional growth.
+  </p>
 </div>
-<h1>hello</h1>
-<div style={{padding:"100px",display:"flex",width:"200px",height:"200px",backgroundColor:"red"}}>
-
-</div>
-<div style={{padding:"100px",display:"flex",width:"200px",height:"200px",backgroundColor:"red"}}>
-
-</div>
-<div style={{padding:"100px",display:"flex",width:"200px",height:"200px",backgroundColor:"red"}}>
-
 </div>
 </div>
 </>
--- react/src/components/Navbar.jsx
+++ react/src/components/Navbar.jsx
@@ -1,36 +1,69 @@
 
-function Navbar({setPage}){
-    return (
-<nav style={{
-    backgroundColor:"black",
-    position:"fixed",
-    top:"0",
-    left:"0",
-    color:"white",
-    padding:"15px",
-    width:"100%",
-    display:"flex",
-    justifyContent:"space-between",
-    alignItems:"center",
-     boxSizing: "border-box" 
-    }}>
-<div>
-    <img src="" alt="companylogo" style={{ height: "30px" }} />
+import "./navbar.css";
+import {
+    FaPhoneAlt,
+    FaEnvelope,
+    FaInstagram,
+    FaFacebookF,
+    FaLinkedinIn,
     
-</div>
-<ul style={{
-    display:"flex",
-    gap:"25px",
-    listStyle:"none",
-    justifyContent:"center",
+} from "react-icons/fa";
 
-    }}>
+function Navbar({ setPage }) {
+    return (
+        <div className="nav-container">
 
-    <li onClick={()=>setPage("Home")} style={{cursor:"pointer"}}>Home</li>
-    <li onClick={()=>setPage("About")} style={{cursor:"pointer"}}>About</li>
-     <li onClick={()=>setPage("Contact")} style={{cursor:"pointer"}}>Contact</li>
-</ul>
-</nav>
+            {/* 🔵 TOP BAR */}
+            <div className="topbar">
+                <div className="left">
+                    <span className="item">
+                        <FaPhoneAlt /> +91 75980 98675
+                    </span>
+
+                    <span className="divider"></span>
+
+                    <span className="item">
+                        <FaEnvelope /> azhagiyamandapam.tn@gteceducation.com
+                    </span>
+                </div>
+
+                <div className="right">
+                    <span>Follow us:</span>
+                    <FaInstagram />
+                    <FaFacebookF />
+                    <FaLinkedinIn />
+                </div>
+            </div>
+
+            {/* ⚪ MAIN NAVBAR */}
+            <div className="mainnav">
+
+                {/* LOGO */}
+                <div className="logo" onClick={() => setPage("Home")}>
+                    <img src="/logo.webp" alt="logo" />
+                </div>
+
+                {/* MENU */}
+                <ul className="menu">
+                
+                    <li className="dropdown">
+                        <span onClick={() => setPage("Course")}>Courses</span>
+                        <ul className="dropdown-menu">
+                            <li onClick={() => setPage("It/Technical")}>It/Technical</li>
+                            <li onClick={() => setPage("NonTechnical")}>Non Technical</li>
+                            <li onClick={() => setPage("Designing")}>Designing</li>
+                            <li onClick={() => setPage("Accounting")}>Accounting</li>
+                            <li onClick={() => setPage("Civil")}>Civil</li>
+                        </ul>
+                    </li>
+                            <li onClick={() => setPage("About")}>About us</li>
+                    <li onClick={() => setPage("Contact")}>Contact us</li>
+                    <li onClick={() => setPage("About")}><button className="bg-blue-500 py-2 px-1 rounded-full">Login</button></li>
+                     <li onClick={() => setPage("Enrollement")}>Enroll</li>
+                </ul>
+            </div>
+        </div>
     );
 }
+
 export default Navbar;
